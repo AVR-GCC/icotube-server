f54523ba362d0754dea8dd26cf30ea22c202ab5a
Bar <ogoun.d@gmail.com>
2022-05-20 21:16:35 +0300

    google login with passport js - debug 13 - revert

--- passport.js
+++ passport.js
@@ -13,40 +13,37 @@ passport.use(new GoogleStrategy({
     console.log('in the "use" function');
     // console.log('accessToken', accessToken);
     // console.log('refreshToken', refreshToken);
-    console.log('profile', profile);
-    // const emails = profile.emails.map(email => email.value) || [null];
-    // const imageUrl = (profile?.photos || [null])[0]?.value;
-    // User.findOne({ email: { $in: emails } }).then(potentialUser => {
-    //   let user = potentialUser;
-    //   if (!user) {
-    //     user = new User({
-    //       email: emails[0],
-    //       imageUrl
-    //     });
-    //   } else if (!user.imageUrl) {
-    //     user.imageUrl = imageUrl;
-    //   }
-    //   user.save().then(() => {
-    //     console.log('done', done);
-    //     console.log('end user', user);
-    //     return done(null, user);
-    //   })
-    // });
-    return done(null, profile);
+    // console.log('profile', profile);
+    const emails = profile.emails.map(email => email.value) || [null];
+    const imageUrl = (profile?.photos || [null])[0]?.value;
+    User.findOne({ email: { $in: emails } }).then(potentialUser => {
+      let user = potentialUser;
+      if (!user) {
+        user = new User({
+          email: emails[0],
+          imageUrl
+        });
+      } else if (!user.imageUrl) {
+        user.imageUrl = imageUrl;
+      }
+      user.save().then(() => {
+        console.log('done', done);
+        console.log('end user', user);
+        return done(null, user);
+      })
+    });
   }
 ));
 
 passport.serializeUser((user, done) => {
   console.log('serializeUser', user);
-  // done(null, user._id);
-  done(null, user);
+  done(null, user._id);
 });
 
-passport.deserializeUser((user, done) => {
-  console.log('deserializeUser', user);
-  // User.findById(id).then(user => {
-  //   console.log('deserializeUser cb user', user);
-  //   return done(null, user);
-  // });
-  return done(null, user);
+passport.deserializeUser((id, done) => {
+  console.log('deserializeUser', id);
+  User.findById(id).then(user => {
+    console.log('deserializeUser cb user', user);
+    return done(null, user);
+  });
 });
