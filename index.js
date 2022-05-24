bcb244de071fbc6fc99d230d139a509116b9083f
Bar <ogoun.d@gmail.com>
2022-05-24 17:03:15 +0300

    google login with passport js - debug 18 - return to express session

--- index.js
+++ index.js
@@ -8,7 +8,8 @@ const cookieParser = require('cookie-parser');
 const passport = require('passport');
 const path = require('path');
 require('./passport.js');
-const cookieSession = require('cookie-session');
+// const cookieSession = require('cookie-session');
+const session = require('express-session');
 // const SQLiteStore = require('connect-sqlite3')(session);
 const postsRouter = require('./routes/posts');
 const paymentsRouter = require('./routes/payments');
@@ -25,13 +26,18 @@ const corsConfig = {
 
 // session
 
-app.use(express.static(path.join(__dirname, 'public')));
-app.use(cookieSession({ name: "session", keys: ["lama"], maxAge: oneDay }));
-app.use(cors(corsConfig));
-app.options('*', cors(corsConfig));
-
 app.use(bodyParser.json());
 app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+// app.use(cookieSession({ name: "session", keys: ["lama"], maxAge: oneDay }));
+app.use(session({
+    secret: "thisismysecrctekeyfhrgfgrfrty84fwir767",
+    saveUninitialized:true,
+    cookie: { maxAge: oneDay },
+    resave: false 
+}));
+app.use(cors(corsConfig));
+app.options('*', cors(corsConfig));
 
 // ------------ MIDDLEWARE ------------
 
